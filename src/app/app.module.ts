897a71f0b8d0643f47bfa6771c0f2f9c18300768
yogeshgarad <yogeshgarad@alohatechnologydev.com>
2019-12-13 12:11:47 +0530

    added login functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,35 @@
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { ApiServiceService } from './api-service.service';
+
+import { AddUserComponent } from './user/add-user/add-user.component';
+import { EditUserComponent } from './user/edit-user/edit-user.component';
+import { ListUserComponent } from './user/list-user/list-user.component';
+
+
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+
 import { CartService } from './cart.service';
 import { CartComponent } from './cart/cart.component';
+
 import { HttpClientModule } from '@angular/common/http';
 import { ShippingComponent } from './shipping/shipping.component';
+
 import { TestComponent } from './test/test.component';
- import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './login/login.component';
+
 import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+
  import {MatFormFieldModule, MatSelectModule} from '@angular/material';
+
 
 @NgModule({
   imports: [
@@ -25,7 +39,6 @@ import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform
     HttpClientModule,
     ReactiveFormsModule,
     MatSelectModule,
-    // MatInputModule,
     MatFormFieldModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
@@ -44,10 +57,13 @@ import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform
     CartComponent,
     ShippingComponent,
     TestComponent,
-    LoginComponent
+    LoginComponent,
+    ListUserComponent,
+    AddUserComponent,
+    EditUserComponent
   ],
   bootstrap: [ AppComponent ],
-  providers: [CartService]
+  providers: [CartService, ApiServiceService]
 })
 export class AppModule { }
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {ApiService} from "../service/api.service";
 
 @Component({
   selector: 'app-login',
@@ -6,12 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor() {
 
-   }
+  loginForm: FormGroup;
+  invalidLogin: boolean = false;
+  constructor(private formBuilder: FormBuilder, private router: Router, private apiService: ApiService) { }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+    const loginPayload = {
+      username: this.loginForm.controls.username.value,
+      password: this.loginForm.controls.password.value
+    }
+    this.apiService.login(loginPayload).subscribe(data => {
+      debugger;
+      if(data.status === 200) {
+        window.localStorage.setItem('token', data.result.token);
+        this.router.navigate(['list-user']);
+      }else {
+        this.invalidLogin = true;
+        alert(data.message);
+      }
+    });
+  }
 
   ngOnInit() {
-    
+    window.localStorage.removeItem('token');
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.required]
+    });
   }
 
 }
